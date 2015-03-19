dc43b85b90ededb9a60fb770209c62f9115ff567
Ron <ron@lisles.net>
2015-03-19 06:33:43 -0500

    Create library project for FuelGaugeKit

---
+++ FuelGaugeKit/FuelGaugeKit/FuelGaugeKit.h
@@ -0,0 +1,19 @@
+//
+//  FuelGaugeKit.h
+//  FuelGaugeKit
+//
+//  Created by Ron Lisle on 3/19/15.
+//  Copyright (c) 2015 Ron Lisle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FuelGaugeKit.
+FOUNDATION_EXPORT double FuelGaugeKitVersionNumber;
+
+//! Project version string for FuelGaugeKit.
+FOUNDATION_EXPORT const unsigned char FuelGaugeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FuelGaugeKit/PublicHeader.h>
+
+
